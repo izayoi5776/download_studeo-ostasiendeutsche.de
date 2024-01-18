39091e879a8c44d88fcfb8ebc02cc62b1d6bb10f
zxd <izayoi5776@gmail.com>
2024-01-18 19:29:02 +0900

    Update app2.py
    增加了换页的对应
    增加了错误处理
    修改了一些log

--- app2.py
+++ app2.py
@@ -1,11 +1,11 @@
 from encodings import utf_8
 from genericpath import exists
 import urllib
+import urllib.request
 import os
 from bs4 import BeautifulSoup
 from pathlib import Path
 import pickle
-import json
 
 # https://studeo-ostasiendeutsche.de/fotothek/china/nanjing/1937-2/1250-p6864
 # 保存 description 为 html 文件
@@ -16,6 +16,7 @@ def writeDesc(url, s):
   fn = path[1:] + ".html"
   s = '<div class="description" url="' + url + '">' + str(s) + "</div>" 
   print(" desc=" + fn)
+  Path(os.path.dirname(fn)).mkdir(parents=True, exist_ok=True)
   with open(fn, "w", encoding="utf-8") as f:
     f.write(str(s))
 
@@ -26,13 +27,17 @@ def getJpg(url):
   #p2 = path.split("/")[-2]  # 1250-p6864
   #p3 = p2.split("-")[-1]    # p6864
   fn = path[1:-5] + ".jpg"
-  print(" fn=" + fn, end="")
+  print(" jpg =" + fn, end="")
   if(os.path.exists(fn)):
     print("...SKIP")
   else:
     print("...GET")
     Path(os.path.dirname(fn)).mkdir(parents=True, exist_ok=True)
-    urllib.request.urlretrieve(url, fn)
+    try:
+      urllib.request.urlretrieve(url, fn)
+    except Exception as err:
+      print("FAIL", err)
+
 
 # 读取任意url
 def get1url(url):
@@ -43,8 +48,21 @@ def get1url(url):
     # 取下层URL
     for i in soup.select("a.koowa_media__item__link"):
       url2 = urllib.parse.urljoin(url, i["href"]) 
-      print(" add " + url2)
+      print(" add sub  " + url2)
       urls.add(url2)
+
+    # 取导航URL
+    for i in soup.select("div.k-pagination a"):
+      try:
+        url2 = urllib.parse.urljoin(url, i["href"]) 
+      except:
+        # 当前页取不到href，跳过
+        continue
+      if url2 in done:
+        print(" not add navi " + url2)
+      else:
+        print(" add navi " + url2)
+        urls.add(url2)
 
     # 图片URL，应该只有一个
     for i in soup.select("a.docman_download__button"):
@@ -54,16 +72,6 @@ def get1url(url):
     # 说明文字，应该只有一个
     for i in soup.select('div[itemprop="description"]'):
       writeDesc(url, str(i))
-
-def printSize():
-  print("todo:" + str(len(urls)) + ", done:" + str(len(done)) + " skip:" + str(len(skip)))
-
-#https://stackoverflow.com/questions/22281059/set-object-is-not-json-serializable
-# convert set to list before save as json
-def set_default(obj):
-    if isinstance(obj, set):
-        return list(obj)
-    raise TypeError
 
 # INSTALL
 '''
@@ -78,27 +86,20 @@ pip install html5lib
 # --- MAIN ----
 # 例：真入口
 urls = {"https://studeo-ostasiendeutsche.de/fotothek/china"}
+# 例：错误页
+#urls = {"https://studeo-ostasiendeutsche.de/fotothek/china/hankow/2005"}
 # 例：带下层和图一览的页面
 #urls = {"https://studeo-ostasiendeutsche.de/fotothek/china/nanjing/1937-2"}
 # 例：图和说明的页面
 #urls = {"https://studeo-ostasiendeutsche.de/fotothek/china/nanjing/1937-2/1250-p6864"}
-# 例：有错的
-#urls = {"https://studeo-ostasiendeutsche.de/fotothek/china/shanghai/1919-1/356-p0526"}
-
 done = set()
-skip = set()
-settingFileName = "setting.json"
+settingFileName = "pickle.dump"
 
 # read setting
 if(os.path.exists(settingFileName)):
-  with open(settingFileName, 'r') as f:
-    js = json.load(f)
-    urls = set(js["urls"])
-    done = set(js["done"])
-    skip = set(js["skip"])
-    print("restore data from last run, ", end="")
-    printSize()
-
+  with open(settingFileName, 'rb') as f:
+    urls, done = pickle.load(f)
+    print("restore data from last run, todo:" + str(len(urls)) + ", done:" + str(len(done)))
 while len(urls)>0:
   url = urls.pop()
   if url in done:
@@ -106,19 +107,12 @@ while len(urls)>0:
   else:
     try:
       get1url(url)
-      done.add(url)
-    except Exception as e:
-      print(" " + str(e) + " url=" + url)
-      skip.add(url)
-    finally:
-      printSize()
-
+    except Exception as err:
+      print("skip err " + url)
+    done.add(url)
+    print("todo:" + str(len(urls)) + ", done:" + str(len(done)))
   # write setting
-  with open(settingFileName, 'w') as f:
-    js = {}
-    js["urls"] = urls
-    js["done"] = done
-    js["skip"] = skip
-    json.dump(js, f, default=set_default)
+  with open(settingFileName, 'wb') as f:
+    pickle.dump([urls, done], f)
 
 
